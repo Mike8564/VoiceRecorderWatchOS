4896751ec4a7060524d2fd3aa68ac658cbb8581d
Mike Sedaghatnia <msedaghatnia85@gmail.com>
2021-02-21 11:32:56 -0500

    Initial Commit

---
+++ VoiceRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceRecorder
+//
+//  Created by Mike Sedaghatnia on 2021-02-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
